11b59d2f6a68efb54230c55480c830f046df9265
brayan-coy <brayan324554@gmail.com>
2024-01-22 20:17:08 -0500

    first commit

--- solicitarcodigo.js
+++ solicitarcodigo.js
@@ -1,14 +1,16 @@
-function solicitarCodigo() {
+function solicitarCodigo(event) {
   // Pide al usuario que ingrese el código
   var codigoIngresado = prompt("Ingresa el código:");
 
   // Verifica si el código ingresado es correcto (puedes cambiar "codigoCorrecto" por el código que desees)
-  if (codigoIngresado === "2345") {
+  if (codigoIngresado === "713") {
     // Si es correcto, redirige al usuario a universo.html
     alert("Código correcto. ¡Bienvenido al viaje al Universo!");
-    window.location.replace("universo.html");
+    // Puedes descomentar la siguiente línea si deseas permitir que el enlace funcione después de ingresar el código correcto
+    // window.location.replace("universo.html");
   } else {
-    // Si es incorrecto, redirige al usuario a index.html
-    alert("Código incorrecto. Serás redirigido al inicio.");
+    // Si es incorrecto, muestra un mensaje de error y evita la acción predeterminada del enlace
+    alert("Código incorrecto. No tienes acceso a esta página.");
+    event.preventDefault(); // Evita que el enlace realice su acción predeterminada
   }
 }
