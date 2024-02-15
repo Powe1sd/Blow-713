46a7906ef53889d9402c8f01ded1a7f1c0fc35ba
brayan-coy <brayan324554@gmail.com>
2024-02-14 20:32:52 -0500

    first commit

--- codigoacces.js
+++ codigoacces.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   swal({
     title: "Hola",
-    text: "Hay un súper descuento para el curso de inglés antes $85.000, ahora lo puedes obtener por tan solo $68.000",
+    text: "Hay un súper descuento para el curso de inglés antes $85.000, ahora lo puedes obtener por tan solo $69.900",
     icon: "success",
     button: "Aceptar",
   });
