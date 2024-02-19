29206961307ca614e7d9b65ec75edcfe0a08d2d1
brayan-coy <brayan324554@gmail.com>
2024-02-19 13:27:58 -0500

    first commit

--- emogi/app.js
+++ emogi/app.js
@@ -1,7 +1,9 @@
 document.addEventListener("mousemove", handleMove);
-document.addEventListener("touchmove", handleMove);
+document.addEventListener("touchmove", handleMove, { passive: false }); // Agregamos { passive: false } para poder prevenir el scroll
 
 function handleMove(event) {
+  event.preventDefault(); // Prevenir el comportamiento predeterminado del evento táctil (scroll)
+
   const face = document.getElementById("face");
   const eyes = document.querySelector(".eyes");
   const pupils = document.querySelectorAll(".pupil");
@@ -18,21 +20,8 @@ function handleMove(event) {
   const boundingBox = face.getBoundingClientRect();
   const offsetX = boundingBox.left + boundingBox.width / 2;
   const offsetY = boundingBox.top + boundingBox.height / 2;
-  const eyeX = boundingBox.left + boundingBox.width / 2;
-  const eyeY = boundingBox.top + boundingBox.height / 2;
 
-  const eyeMovementRange = 28; // Rango de movimiento de los ojos
-
-  const deltaX = mouseX - offsetX;
-  const deltaY = mouseY - offsetY;
-  const angle = Math.atan2(deltaY, deltaX);
-  const distance = Math.min(
-    Math.sqrt(deltaX * deltaX + deltaY * deltaY),
-    boundingBox.width / 4
-  ); // Limitar el movimiento al radio del ojo
-
-  const eyeMovementX = Math.cos(angle) * eyeMovementRange;
-  const eyeMovementY = Math.sin(angle) * eyeMovementRange;
+  const eyeMovementRange = 10; // Rango de movimiento de los ojos
 
   pupils.forEach((pupil) => {
     const pupilBoundingBox = pupil.getBoundingClientRect();
@@ -58,6 +47,13 @@ function handleMove(event) {
       eyeMovementRange
     )}px)`; // Mover los iris limitando su movimiento al radio del ojo
   });
+
+  const deltaX = mouseX - offsetX;
+  const deltaY = mouseY - offsetY;
+  const angle = Math.atan2(deltaY, deltaX);
+
+  const eyeMovementX = Math.cos(angle) * eyeMovementRange;
+  const eyeMovementY = Math.sin(angle) * eyeMovementRange;
 
   eyes.style.transition = "transform 0.2s ease"; // Aplicar una transición suave
   eyes.style.transform = `translate(${eyeMovementX}px, ${eyeMovementY}px)`; // Mover los ojos
