2fdb0d43a639761d4e18df1dfd3db4ff625b304a
brayan-coy <brayan324554@gmail.com>
2024-04-19 21:47:39 -0500

    first commit

---
+++ Tienda/ocultarseccio.js
@@ -0,0 +1,6 @@
+/* Ocultar la seccion de GORRAS */
+// Seleccionar el elemento que contiene la sección de camisas
+var shirtsSection = document.querySelector(".categorySection");
+
+// Ocultar la sección de camisas estableciendo su estilo de visualización en 'none'
+shirtsSection.style.display = "none";
